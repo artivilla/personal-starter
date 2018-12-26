e5120a475570282a9f0de00e65a2bb57af136bcf
Arti Villa <artivillachandok@gmail.com>
2018-12-26 03:37:44 -0800

    feat: generates value for dynamic classnames

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	testEnvironment: 'jest-environment-jsdom',
 	setupTestFrameworkScriptFile: require.resolve('./test/jest.setup.js'),
 	moduleNameMapper: {
+		'\\.module\\.css$': 'identity-obj-proxy',
 		'\\.css$': require.resolve('./test/style-mock.js'),
 	},
 }
